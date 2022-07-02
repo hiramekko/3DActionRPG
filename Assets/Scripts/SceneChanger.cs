fc7a94d188122066068ef53f8ce12759b50c4f38
Cactusism1 <sisimple99.81@gmail.com>
2022-07-02 15:26:40 +0900

    シーン遷移処理の追加

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
