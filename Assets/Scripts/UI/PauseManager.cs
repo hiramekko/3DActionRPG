b05b89ceb10b53232e485c635e509d5c6407c124
Cactusism1 <sisimple99.81@gmail.com>
2023-01-11 23:21:41 +0900

    ポーズ機能を追加

---
+++ Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ポーズ機能の管理
+/// </summary>
+public class PauseManager : MonoBehaviour
+{
+    bool _isPause = false;
+    public delegate void Pause(bool isPause);
+    Pause _onPauseResume = default;
+
+    public Pause OnPauseResume
+    {
+        get { return _onPauseResume; }
+        set { _onPauseResume = value; }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            PauseResume();
+        }
+    }
+
+    void PauseResume()
+    {
+        _isPause = !_isPause;
+        _onPauseResume(_isPause);
+    }
+}
---
+++ Assets/Scripts/UI/PauseUIManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ポーズ時にポーズしているかを確認するUIの表示
+/// </summary>
+public class PauseUIManager : MonoBehaviour
+{
+    [SerializeField] Animator _pauseAnimator = default;
+    PauseManager _pm = default;
+
+    void Awake()
+    {
+        _pm = GameObject.FindObjectOfType<PauseManager>();
+    }
+
+    void OnEnable()
+    {
+        _pm.OnPauseResume += ShowMessage;
+    }
+
+    void OnDisable()
+    {
+        _pm.OnPauseResume -= ShowMessage;
+    }
+
+    void ShowMessage(bool isPause)
+    {
+        if (isPause)
+        {
+            _pauseAnimator.Play("Pause");
+            Debug.Log("ポーズ中");
+        }
+        else
+        {
+            _pauseAnimator.Play("Resume");
+            Debug.Log("再開");
+        }
+    }
+}
