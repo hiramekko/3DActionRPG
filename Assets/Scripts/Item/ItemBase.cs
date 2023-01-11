fab3f357bed50822ab195980515039aa8b40881e
Cactusism1 <sisimple99.81@gmail.com>
2023-01-11 23:04:42 +0900

    アイテムの基底クラスを追加

---
+++ Assets/Scripts/Item/ItemBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// アイテムの基底クラス
+/// </summary>
+
+public abstract class ItemBase : MonoBehaviour
+{
+    public abstract void Activate();
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("当たった");
+            Activate();
+        }
+    }
+
+    protected void Destroy() => Destroy(gameObject);
+}
